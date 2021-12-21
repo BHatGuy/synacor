5ae0f55f9c97d35c39d1a28d02b3a0e82c35cbd9
Malte Müller <malte@malte-mueller.eu>
2021-12-21 20:47:31 +0100

    Move debugger to own file

---
+++ src/debugger.rs
@@ -0,0 +1,98 @@
+use crate::machine::Machine;
+use std::fs;
+use std::io::Write;
+use std::sync::mpsc;
+
+pub struct Debugger {
+    tx: mpsc::Sender<String>,
+    rx: mpsc::Receiver<String>,
+    running: bool,
+}
+
+impl Debugger {
+    pub fn new(tx: mpsc::Sender<String>, rx: mpsc::Receiver<String>) -> Self {
+        Self {
+            tx,
+            rx,
+            running: false,
+        }
+    }
+
+    pub fn debugger_step(&mut self, m: &mut Machine) {
+        if self.running {
+            while let Ok(command) = self.rx.try_recv() {
+                self.process_comand(command, m);
+            }
+        } else {
+            while !self.running {
+                if let Ok(command) = self.rx.recv() {
+                    let ss = self.process_comand(command, m);
+                    if ss {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    fn process_comand(&mut self, cmd: String, m: &mut Machine) -> bool {
+        let command: Vec<&str> = cmd.split(" ").collect();
+        let answer;
+        let mut ss = false;
+        match command[0] {
+            "dump" => {
+                let mut file = fs::File::create("state.bin").unwrap();
+                let bytes = m.dump();
+                file.write_all(&bytes).unwrap();
+                answer = format!("dumped to state.bin ({} bytes)", bytes.len());
+            }
+            "restore" => {
+                if let Ok(bytes) = fs::read("state.bin") {
+                    m.restore(&bytes);
+                    answer = format!("restored state.bin ({} bytes)", bytes.len());
+                } else {
+                    answer = "Cant read state".to_owned();
+                }
+            }
+            "set" => {
+                if command.len() == 3 {
+                    if let (Ok(idx), Ok(val)) = (command[1].parse(), command[2].parse()) {
+                        m.set_reg(idx, val);
+                        answer = format!("Set reg {} to {:#x}", idx, val);
+                    } else {
+                        answer = format!("Invalid set command! {:?}", command);
+                    }
+                } else {
+                    answer = format!("Invalid set command! {:?}", command);
+                }
+            }
+            "get" => {
+                if command.len() == 2 {
+                    if let Ok(idx) = command[1].parse() {
+                        let val = m.get_register(idx);
+                        answer = format!("reg[{}]={:#x}", idx, val);
+                    } else {
+                        answer = format!("Invalid get command! {:?}", command);
+                    }
+                } else {
+                    answer = format!("Invalid get command! {:?}", command);
+                }
+            }
+            "stop" => {
+                self.running = false;
+                answer = format!("stopped");
+            }
+            "c" => {
+                self.running = true;
+                answer = format!("continued");
+            }
+            "" => {
+                ss = true;
+                answer = format!("{:#06x}: {}", m.pc, m.fetch());
+            }
+            _ => answer = format!("Unknown command! {:?}", command),
+        };
+        self.tx.send(answer).unwrap();
+        ss
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod machine;
+mod debugger;
 use machine::Machine;
+use debugger::Debugger;
 use std::env;
 use std::fs;
 use std::io::Write;
@@ -13,11 +15,13 @@ const SOCKET: &str = "/tmp/synacor.sock";
 fn handle_client(stream: UnixStream, tx: mpsc::Sender<String>, rx: mpsc::Receiver<String>) {
     let mut stream_out = stream.try_clone().unwrap();
     let stream = BufReader::new(stream);
+    write!(stream_out, "> ");
     for line in stream.lines() {
         let line = line.unwrap();
         tx.send(line).unwrap();
         let answer = rx.recv().unwrap();
         writeln!(stream_out, "{}", answer).unwrap();
+        write!(stream_out, "> ").unwrap();
     }
 }
 
@@ -25,65 +29,6 @@ fn cleanup() {
     fs::remove_file(SOCKET).unwrap();
 }
 
-fn process_comand(cmd: String, m: &mut Machine, running: &mut bool) -> String {
-    let command: Vec<&str> = cmd.split(" ").collect();
-    // TODO break, single step, continue
-    match command[0] {
-        "dump" => {
-            let mut file = fs::File::create("state.bin").unwrap();
-            let bytes = m.dump();
-            file.write_all(&bytes).unwrap();
-            return format!("dumped to state.bin ({} bytes)", bytes.len());
-        }
-        "restore" => {
-            let bytes = match fs::read("state.bin") {
-                Ok(b) => b,
-                Err(_) => {
-                    return "Cant read state".to_owned();
-                }
-            };
-            m.restore(&bytes);
-            return format!("restored state.bin ({} bytes)", bytes.len());
-        }
-        "set" => {
-            if command.len() == 3 {
-                if let (Ok(idx), Ok(val)) = (command[1].parse(), command[2].parse()) {
-                    m.set_reg(idx, val);
-                    return format!("Set reg {} to {:#x}", idx, val);
-                } else {
-                    return format!("Invalid set command! {:?}", command);
-                }
-            } else {
-                return format!("Invalid set command! {:?}", command);
-            }
-        }
-        "get" => {
-            if command.len() == 2 {
-                if let Ok(idx) = command[1].parse() {
-                    let val = m.get_register(idx);
-                    return format!("reg[{}]={:#x}", idx, val);
-                } else {
-                    return format!("Invalid get command! {:?}", command);
-                }
-            } else {
-                return format!("Invalid get command! {:?}", command);
-            }
-        }
-        "stop" => {
-            *running = false;
-            return format!("stopped");
-        }
-        "c" => {
-            *running = true;
-            return format!("continued");
-        }
-        "" => {
-            m.step();
-            return format!("{:#06x}: {}", m.pc, m.fetch());
-        }
-        _ => return format!("Unknown command! {:?}", command),
-    }
-}
 
 fn main() {
     ctrlc::set_handler(move || {
@@ -105,30 +50,20 @@ fn main() {
         }
     };
 
-    for arg in env::args().skip(1) {
-        let bytes = match fs::read(&arg) {
-            Ok(b) => b,
-            Err(_) => {
-                continue;
-            }
-        };
-        let mut m = Machine::new(bytes);
-        let mut running = false;
-        while !m.halted() {
-            if running {
-                while let Ok(command) = rx.try_recv() {
-                    let answer = process_comand(command, &mut m, &mut running);
-                    tx.send(answer).unwrap();
-                }
-                m.step();
-            } else {
-                if let Ok(command) = rx.recv() {
-                    let answer = process_comand(command, &mut m, &mut running);
-                    tx.send(answer).unwrap();
-                }
-            }
-            
+    let bytes = match fs::read(env::args().skip(1).next().unwrap()) {
+        Ok(b) => b,
+        Err(_) => {
+            return
         }
+    };
+    let mut m = Machine::new(bytes);
+    let mut debugger = Debugger::new(tx, rx);
+
+    while !m.halted() {
+        debugger.debugger_step(&mut m);
+        m.step();
     }
+
+
     cleanup();
 }
